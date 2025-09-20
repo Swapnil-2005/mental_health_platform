9c509f5059a4ed66608e299ad16eff747069f24c
Swapnil-2005 <swapnilbanerjee183@gmail.com>
2025-09-20 10:18:29 +0530

    folder stucture added

---
+++ template.sh
@@ -0,0 +1,20 @@
+#creating directories
+mkdir -p src
+mkdir -p research
+mkdir -p data
+mkdir -p static
+mkdir -p templates
+#creating files
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trials.ipynb
+touch requirements.txt
+touch setup.py
+touch store_index.py
+
+#echo command
+echo "Directory structure created successfully!."
